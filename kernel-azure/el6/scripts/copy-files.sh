a97cb82b91e9fa9125d00cf78bba6c697b577527
Stephen A. Zarkos <obsid@sentry.net>
2018-06-27 17:01:05 -0700

    Upload el6 files

---
+++ kernel-azure/el6/scripts/copy-files.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# CentOS6
+## Note: Does not yet include vmsock and pci-hyperv
+
+[ ! -z "$1" ] || { echo "Error: copy-files: kversion undefined" ; exit 1; }
+[ ! -z "$2" ] || { echo "Error: copy-files: lisdir undefined" ; exit 1; }
+
+kversion="${1}"
+lisdir="${2}"
+
+topdir="/mnt/resource/CENTOS-KERNEL"
+lisdir="/home/ostc/CENTOS-KERNEL/${lisdir}/hv-rhel6.x/hv"
+kerndir="/home/ostc/CENTOS-KERNEL/linux-${kversion}.lis"
+
+# Fixup mshyperv.c headers
+sed -i 's/<asm\/hyperv\.h>/"hyperv.h"/'				${kerndir}/arch/x86/kernel/cpu/mshyperv.c
+sed -i 's/<asm\/mshyperv\.h>/"mshyperv.h"/'			${kerndir}/arch/x86/kernel/cpu/mshyperv.c
+cp ${kerndir}/arch/x86/include/asm/mshyperv.h			${kerndir}/arch/x86/kernel/cpu/mshyperv.h
+cp ${kerndir}/arch/x86/include/asm/hyperv.h			${kerndir}/arch/x86/kernel/cpu/hyperv.h
+
+
+# hv_vmbus
+cp ${lisdir}/channel.c						${kerndir}/drivers/hv/channel.c
+cp ${lisdir}/channel_mgmt.c					${kerndir}/drivers/hv/channel_mgmt.c
+cp ${lisdir}/connection.c					${kerndir}/drivers/hv/connection.c
+cp ${lisdir}/hv_balloon.c					${kerndir}/drivers/hv/hv_balloon.c
+cp ${lisdir}/hv.c						${kerndir}/drivers/hv/hv.c
+cp ${lisdir}/hv_fcopy.c						${kerndir}/drivers/hv/hv_fcopy.c
+cp ${lisdir}/hv_kvp.c						${kerndir}/drivers/hv/hv_kvp.c
+cp ${lisdir}/hv_util.c						${kerndir}/drivers/hv/hv_util.c
+cp ${lisdir}/hv_snapshot.c					${kerndir}/drivers/hv/hv_snapshot.c
+cp ${lisdir}/hv_utils_transport.c				${kerndir}/drivers/hv/hv_utils_transport.c
+cp ${lisdir}/hv_utils_transport.h				${kerndir}/drivers/hv/hv_utils_transport.h
+cp ${lisdir}/hyperv_vmbus.h					${kerndir}/drivers/hv/hyperv_vmbus.h
+cp ${lisdir}/ring_buffer.c					${kerndir}/drivers/hv/ring_buffer.c
+cp ${lisdir}/vmbus_drv.c					${kerndir}/drivers/hv/vmbus_drv.c
+cp ${lisdir}/hv_trace.c						${kerndir}/drivers/hv/hv_trace.c
+cp ${lisdir}/hv_trace.h						${kerndir}/drivers/hv/hv_trace.h
+
+ # Moving these from arch/x86/hyperv to build correctly. Only vmbus requires them anyway
+ cp ${lisdir}/arch/x86/hyperv/hv_init.c				${kerndir}/drivers/hv/hv_init.c
+ sed -i 's/lis\/asm\/mshyperv\.h/asm\/mshyperv\.h/'		${kerndir}/drivers/hv/hv_init.c
+ sed -i 's/lis\/asm\/hyperv\.h/asm\/hyperv\.h/'			${kerndir}/drivers/hv/hv_init.c
+
+ cp ${lisdir}/arch/x86/hyperv/ms_hyperv_ext.c			${kerndir}/drivers/hv/ms_hyperv_ext.c
+ sed -i 's/lis\/asm\/mshyperv\.h/asm\/mshyperv\.h/'		${kerndir}/drivers/hv/ms_hyperv_ext.c
+ sed -i 's/lis\/asm\/hyperv\.h/asm\/hyperv\.h/'			${kerndir}/drivers/hv/ms_hyperv_ext.c
+
+sed -i 's/lis\/asm\/mshyperv\.h/asm\/mshyperv\.h/'	${kerndir}/drivers/hv/channel_mgmt.c
+sed -i 's/lis\/asm\/hyperv\.h/asm\/hyperv\.h/'		${kerndir}/drivers/hv/connection.c
+sed -i 's/lis\/asm\/mshyperv\.h/asm\/mshyperv\.h/'	${kerndir}/drivers/hv/connection.c
+sed -i 's/lis\/asm\/hyperv\.h/asm\/hyperv\.h/'		${kerndir}/drivers/hv/hv.c
+sed -i 's/lis\/asm\/mshyperv\.h/asm\/mshyperv\.h/'	${kerndir}/drivers/hv/hv.c
+sed -i 's/lis\/asm\/mshyperv\.h/asm\/mshyperv\.h/'	${kerndir}/drivers/hv/hv_util.c
+sed -i 's/lis\/asm\/hyperv\.h/asm\/hyperv\.h/' 		${kerndir}/drivers/hv/vmbus_drv.c
+sed -i 's/lis\/asm\/mshyperv\.h/asm\/mshyperv\.h/'	${kerndir}/drivers/hv/vmbus_drv.c
+
+for i in ${kerndir}/drivers/hv/*; do
+	sed -ri 's/"(\.\/)?include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/' $i
+	sed -i 's/"include\/uapi\/linux\/hyperv.h"/<uapi\/linux\/hyperv.h>/' $i
+done
+
+
+# hv_netvsc
+cp ${lisdir}/hyperv_net.h					${kerndir}/drivers/net/hyperv/hyperv_net.h
+cp ${lisdir}/netvsc.c						${kerndir}/drivers/net/hyperv/netvsc.c
+cp ${lisdir}/netvsc_drv.c					${kerndir}/drivers/net/hyperv/netvsc_drv.c
+cp ${lisdir}/rndis_filter.c					${kerndir}/drivers/net/hyperv/rndis_filter.c
+
+sed -ri 's/"(\.\/)?include\/linux\/rndis\.h"/<linux\/rndis\.h>/' ${kerndir}/drivers/net/hyperv/hyperv_net.h
+
+for i in ${kerndir}/drivers/net/hyperv/*; do
+	 sed -ri 's/"(\.\/)?include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/' $i
+done
+
+
+# hv_storvsc
+cp ${lisdir}/storvsc_drv.c					${kerndir}/drivers/scsi/storvsc_drv.c
+sed -i 's/"include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/'	${kerndir}/drivers/scsi/storvsc_drv.c
+
+
+# Hyper-V HID
+cp ${lisdir}/hid-hyperv.c					${kerndir}/drivers/hid/hid-hyperv.c
+sed -i 's/"include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/'	${kerndir}/drivers/hid/hid-hyperv.c
+
+
+# Hyper-V FB
+cp ${lisdir}/hyperv_fb.c					${kerndir}/drivers/video/hyperv_fb.c
+sed -i 's/"include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/'	${kerndir}/drivers/video/hyperv_fb.c
+
+
+# Hyper-V Keyboard
+cp ${lisdir}/hyperv-keyboard.c					${kerndir}/drivers/input/serio/hyperv-keyboard.c
+sed -i 's/"include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/'	${kerndir}/drivers/input/serio/hyperv-keyboard.c
+
+
+# Hyper-V UIO
+cp ${lisdir}/uio_hv_generic.c					${kerndir}/drivers/uio/uio_hv_generic.c
+sed -i 's/"include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/'	${kerndir}/drivers/uio/uio_hv_generic.c
+cp ${lisdir}/hyperv_vmbus.h					${kerndir}/drivers/uio/hyperv_vmbus.h
+sed -i 's/"include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/'	${kerndir}/drivers/uio/hyperv_vmbus.h
+cp ${lisdir}/hv_trace.h						${kerndir}/drivers/uio/hv_trace.h
+
+
+# Hyper-V network-direct
+#${lisdir}/hvnd_addr.c
+#${lisdir}/mx_abi.h
+#${lisdir}/provider.c
+#${lisdir}/vmbus_rdma.c
+#${lisdir}/vmbus_rdma.h
+#sed -i 's/"include\/linux\/hyperv\.h"/<linux\/hyperv\.h>/'
+
+
+# Headers
+cp ${lisdir}/arch/x86/include/lis/asm/mshyperv.h		${kerndir}/arch/x86/include/asm/mshyperv.h
+cp ${lisdir}/arch/x86/include/uapi/lis/asm/hyperv.h		${kerndir}/arch/x86/include/asm/hyperv.h
+sed -i 's/"uuid\.h"/<linux\/uuid\.h>/'				${kerndir}/arch/x86/include/asm/hyperv.h
+
+cp ${lisdir}/include/linux/hyperv.h				${kerndir}/include/linux/hyperv.h
+cp ${lisdir}/include/linux/hv_compat.h				${kerndir}/include/linux/hv_compat.h
+
+mkdir ${kerndir}/include/uapi/linux
+cp ${lisdir}/include/uapi/linux/hyperv.h			${kerndir}/include/uapi/linux/hyperv.h
+sed -i 's/"uuid\.h"/<linux\/uuid\.h>/'				${kerndir}/include/uapi/linux/hyperv.h
+
+sed -i 's/lis\/asm\/hyperv\.h/asm\/hyperv\.h/'			${kerndir}/arch/x86/include/asm/mshyperv.h
+sed -i 's/lis\/asm\/hyperv\.h/asm\/hyperv\.h/'			${kerndir}/include/linux/hyperv.h
+
+
+# Copy fixed Makefile and Kconfig files
+cp ./Makefile-drivers_hv					${kerndir}/drivers/hv/Makefile
+
---
+++ kernel-azure/el6/scripts/lis-kbuild-cnt6.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+lis="4.2.5"
+kversion=$( curl -s "http://vault.centos.org/6.9/updates/Source/SPackages/" | \
+            grep "kernel-2.6.32-696" | \
+            sed 's/.*<a href="//' | sed 's/">.*//' | sort -V | tail -1 | sed 's/\.src\.rpm//' | sed 's/kernel-//' )
+kbasever=$( echo -n $kversion | sed -r 's/\.[0-9]*\.[0-9]*\.el[6-8]//' )
+
+topdir="/mnt/resource/CENTOS-KERNEL"
+cd $topdir
+
+echo "Building for LIS version $lis and kernel version $kversion ..."
+
+# Download the latest LIS source
+# Output example: lis-next-4.2.5
+echo "Downloading v${lis}.tar.gz..."
+curl -sL https://github.com/LIS/lis-next/archive/${lis}.tar.gz | tar zx
+
+
+# Download the latest CentOS6 kernel SRPM
+echo "Downloading kernel-${kversion}.src.rpm..."
+curl -so ${topdir}/kernel-${kversion}.src.rpm "http://vault.centos.org/6.9/updates/Source/SPackages/kernel-${kversion}.src.rpm"
+echo "Checking GPG signature for kernel-${kversion}.src.rpm..."
+rpm -K kernel-${kversion}.src.rpm
+if [ "$?" -ne "0" ]; then
+	echo "Unable to verify signature of kernel-${kversion}.src.rpm"
+	exit
+fi
+
+
+# Extract the SRPM
+echo "Extracting kernel-${kversion}.src.rpm..."
+rm -rf rpmbuild 2>/dev/null
+mkdir -p ${topdir}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+rpm -i ${topdir}/kernel-${kversion}.src.rpm 2>&1 | grep -v exist
+
+
+# Extract the kernel source
+echo "Setting up kernel source trees..."
+
+cp rpmbuild/SOURCES/linux-${kversion}.tar.bz2 .
+tar -jxf ./linux-${kversion}.tar.bz2
+mv linux-${kversion} linux-${kversion}.orig
+
+cp rpmbuild/SOURCES/linux-${kversion}.tar.bz2 .
+tar -jxf ./linux-${kversion}.tar.bz2
+mv linux-${kversion} linux-${kversion}.lis
+
+
+# Copy files from lis-next-${lis} to linux-${kversion}.lis
+# Fixme: checkout this script and other need things (makefile/kconfig)
+# Fixme: file should accept parameters, i.e. "./copy-files.sh <LISVER> <KERNVER>"
+echo "Copying LIS files to source tree..."
+./copy-files.sh "$kversion" "lis-next-${lis}"
+
+# Create the patch
+echo "Creating the LIS patch, copying to SOURCES..."
+diff -Naur linux-${kversion}.orig linux-${kversion}.lis 2>/dev/null > LIS-${lis}_linux-${kbasever}.patch
+cp LIS-${lis}_linux-${kbasever}.patch ./rpmbuild/SOURCES/
+
+
+# Patch SPEC and configs
+echo "Patching spec file..."
+cd ${topdir}/rpmbuild/SPECS
+
+cp ${topdir}/LIS-kernel-azure.spec.patch-orig ./LIS-kernel-azure.spec.patch
+sed -i "s/LIS\.patch/LIS-${lis}_linux-${kbasever}\.patch/g" ./LIS-kernel-azure.spec.patch
+
+cp kernel.spec kernel.spec.orig
+patch -p0 < ./LIS-kernel-azure.spec.patch
+
+
+# Patch kernel build config
+echo "Patching kernel build config..."
+cd ${topdir}/rpmbuild/SOURCES
+#patch -p0 < ${topdir}/kernel-3.10.0-x86_64.config.patch
+#patch -p0 < ${topdir}/kernel-3.10.0-x86_64-debug.config.patch
+
+
+# Build the kernel
+echo -e "Begin building kernel...\n\n"
+cd ..
+rpmbuild -ba ./SPECS/kernel.spec
+
+
+# Cleanup
+echo "Cleaning up files..."
+cd ${topdir}
+rm -f linux-${kversion}.tar.xz
+rm -rf linux-${kversion}*
+rm -rf lis-next-${lis}
+rm kernel-${kversion}.src.rpm
+rm -f LIS-${lis}_linux-${kbasever}.patch
